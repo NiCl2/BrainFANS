34415a540daaf5873f9e533f0f6c2517578f3410
nicholas clifton <nicholasclifton@MacBook-Pro-10.local>
2023-05-11 13:26:14 +0100

    fitModelsAll.r local

--- array/DNAm/analysis/neuralCellComposition/fitModelsAll.r
+++ array/DNAm/analysis/neuralCellComposition/fitModelsAll.r
@@ -5,8 +5,10 @@
 ## Purpose of script: To train a series of models to predict different combinations of neural cell types
 ##
 ## Author: Eilis Hannon
+## Adapted: Nicholas Clifton
 ##
 ## Date Created: 19/10/2022
+## Date Adapted: 10/05/2023
 ##
 ##---------------------------------------------------------------------#
 
@@ -27,16 +29,24 @@ numProbes<-100 # number of sites included in model
 intervals<-seq(0.1,0.9,0.1) # prop of each cell type for test data
 probeSelect<- "any" 
 
-args<-commandArgs(trailingOnly = TRUE)
-normData<-args[1]
-refPath<-args[2]
-refPanelPath<-args[3]
+# args<-commandArgs(trailingOnly = TRUE)
+# normData<-args[1]
+# refPath<-args[2]
+# refPanelPath<-args[3]
+
+setwd("~/OneDrive - University of Exeter/Documents/methylation/RawData_Mouse_DNAm/")
+normData <- "MouseArray_CellDeconv_FilteredNormalised_Betas.rdat"
+refPath <- "../MouseMethylation-12v1-0_A2.csv"
+refPanelPath <- ""
 
 
 #----------------------------------------------------------------------#
 # LOAD PACKAGES
 #----------------------------------------------------------------------#
+library(dplyr)
+# devtools::install_github("ds420/CETYGO")
 library(CETYGO)
+# devtools::install_github("immunomethylomics/IDOL")
 library(IDOL)
 library(doParallel)
 nworkers <- 2 ## if pushed too high causes OOM errors
@@ -47,11 +57,18 @@ nworkers <- 2 ## if pushed too high causes OOM errors
 #----------------------------------------------------------------------#
 message("Loading data")
 load(normData)
-load(file.path(refPath, "AllProbeIlluminaAnno.Rdata"))
+pheno.all <- sample_sheet
+norm.all <- normbetas
+rm(sample_sheet, normbetas)
 
-colnames(pheno.all)[3]<-"CellType" # need to rename for use with IDOL functions
-pheno.all$CellType<-gsub("\\+", "Pos", pheno.all$CellType) ## need to remove the "+" and "-"
-pheno.all$CellType<-gsub("\\-", "Neg", pheno.all$CellType) ## need to remove the "+" and "-"
+probeAnnot <- read.csv("../MouseMethylation-12v1-0_A2.csv", skip = 7) %>%
+  mutate(probeID = paste(Name, AddressA_ID, sep = "_")) %>%
+  filter(CHR != "") %>%
+  dplyr::select(probeID, Name, CHR, MAPINFO) 
+rownames(probeAnnot) <- probeAnnot$probeID
+
+colnames(pheno.all)[colnames(pheno.all) == "Nuclei_Fraction"] <- "CellType" # need to rename for use with IDOL functions
+pheno.all$CellType[pheno.all$CellType != "NeuN"] <- "NonNeuronal"
 pheno.all$CellType<-factor(pheno.all$CellType)
 
 probeAnnot<-probeAnnot[rownames(norm.all),]
@@ -59,19 +76,6 @@ probeAnnot<-probeAnnot[rownames(norm.all),]
 ## filter to autosome probes
 message("Filtering data")
 norm.all<-norm.all[!probeAnnot$CHR %in% c("X", "Y"),]
-
-## filter out cross hyb & SNP probes
-crosshyb<-read.csv(file.path(refPath, "CrossHybridisingProbesPriceORWeksberg.csv"), row.names = 1)
-probes<-read.csv(file.path(refPath, "SNPsinProbesAnno.csv"), row.names = 1)
-
-## remove cross hybridising probes
-remove<-match(crosshyb[,1], rownames(norm.all))
-remove<-remove[which(is.na(remove) != TRUE)]
-norm.all<-norm.all[-remove,]
-## remove SNP probes
-probes<-probes[row.names(norm.all),]
-norm.all<-norm.all[which(probes$Weksburg_CommonSNP_Af_within10bpSBE == "" & probes$Illumina_CommonSNP_Af_within10bpSBE == ""),]
-
 
 #----------------------------------------------------------------------#
 # SELECT PROBES FOR DECONVOLUTION
@@ -99,19 +103,10 @@ for(i in 1:nrow(refPanels)){
 	matrixSimProp<-matrixSimProp[which(signif(rowSums(matrixSimProp),3) == 1),]
 	colnames(matrixSimProp)<-cellTypes
 	hetBetas <-createBulkProfiles(norm.sub[,exclude[cellTypes]], matrixSimProp) 
-
-	# perform idol dmr test
-	idolDMRs<-CandidateDMRFinder.v2(cellTypes, norm.sub, pheno.sub, M = 150,
-	   equal.variance = F)		
-	## use IDOL method
-	if(length(cellTypes) > 2) {
-		idolLib<-IDOLoptimize(idolDMRs, hetBetas, matrixSimProp*100, libSize = numProbes*length(cellTypes), numCores = nworkers, maxIt = 300)
-		brainCoefIDOL[[i]]<-idolLib$`IDOL Optimized CoefEsts`
-	}
 	
 	## use ANOVA
 	compData <- pickCompProbesMatrix(rawbetas=norm.sub, cellInd=cellInd, cellTypes = cellTypes, numProbes = numProbes/2, probeSelect = probeSelect)
 	brainCoefANOVA[[i]]<-compData$coefEsts
-	save(brainCoefANOVA, brainCoefIDOL, file = file.path(gsub("3_normalised", "4_analysis", dirname(normData)), "CoefBrainModels.rdata"))
+	save(brainCoefANOVA, file = paste(tools::file_path_sans_ext(normData), "CoefBrainModels.rdata", sep = "_"))
 }
 
